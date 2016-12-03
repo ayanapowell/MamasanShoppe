c19b5aebedd316ec42a92c29860247d414ed9a24
ayana powell <ayanapowell1@gmail.com>
2016-12-02 17:05:48 -0800

    add a sliding side navbar for mobile

--- build/scripts/components/responsive-nav.js
+++ build/scripts/components/responsive-nav.js
@@ -2,26 +2,49 @@ var app = app || {};
 (function($){
   app.responsiveNav = {
   	responsiveNaviation : function () {
-      var that = this;
-
+      this.els.responsiveNav.addClass('hidden-nav')
       this.els.initialNav.addClass('hidden').before('<div id="menu">☰</div>');
+
       $('#menu').click(function(){
-        that.els.initialNav.toggleClass('hidden');
+        // alert("jo");
+        $(this).hide();
+        setTimeout(function() {
+          $('.overlays').css({
+            'opacity': '0.8'
+          });
+        }, 80);
+        $('#close-button').show();
+        $('#responsive-nav').removeClass('hidden-nav');
       });
 
+      $('#close-button').click(function() {
+        $(this).hide();
+        $('.overlays').css({
+          'opacity': '0'
+        });
+        $('#menu').show();
+        $('#responsive-nav').addClass('hidden-nav');
+      });
+  	},
+
+
+
+
+    removeAttribute: function () {
       $(window).resize(function(){
       	if(window.innerWidth > 768) {
       		$("#nav").removeAttr("style");
       	}
       });
-  	},
+    },
     init : function() {
       app.responsiveNav.els = {
-    	  initialNav : $('#nav')
+    	  initialNav : $('#nav'),
+        responsiveNav : $('#responsive-nav')
       };
       // console.log('init');
       app.responsiveNav.responsiveNaviation();
-
+      app.responsiveNav.removeAttribute();
     }
   };
   jQuery(document).ready(function() {
